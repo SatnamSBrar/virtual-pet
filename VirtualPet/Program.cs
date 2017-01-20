17d748e82ebd1730f31cff0e7345216e493a5a47
Satnam Brar <satnamsbrar@gmail.com>
2017-01-20 15:46:45 -0500

    version 1.0 - mvp

--- VirtualPet/Program.cs
+++ VirtualPet/Program.cs
@@ -10,6 +10,118 @@ namespace VirtualPet
     {
         static void Main(string[] args)
         {
+            //create object
+            Roscoe vPet = new Roscoe();
+
+            //sets initial values for pet status
+            vPet.Hunger = 0;
+            vPet.Thirst = 0;
+            vPet.Potty = 0;
+            vPet.Energy = 20;
+            vPet.Tired = 0;
+            vPet.Health = 0;
+
+            while (true)
+            {
+                //displays pet status
+                Console.WriteLine("\nHunger:      " + vPet.Hunger);
+                Console.WriteLine("Thirst:      " + vPet.Thirst);
+                Console.WriteLine("Potty        " + vPet.Potty);
+                Console.WriteLine("Energy:      " + vPet.Energy);
+                Console.WriteLine("Sleepiness:  " + vPet.Tired);
+                Console.WriteLine("Health:      " + vPet.Health);
+
+                //prompts user for action
+                Console.WriteLine("\nWhat would you like to do?");
+                Console.WriteLine("Enter a number 1-8");
+                Console.WriteLine("\t1 - Feed");
+                Console.WriteLine("\t2 - Give water");
+                Console.WriteLine("\t3 - Take outside to potty");
+                Console.WriteLine("\t4 - Play");
+                Console.WriteLine("\t5 - Tuck into bed");
+                Console.WriteLine("\t6 - Take to vet");
+                Console.WriteLine("\t7 - Quit");
+                Console.WriteLine("\t8 - Restart\n\n\n\n");
+
+                int input = int.Parse(Console.ReadLine());
+
+                //quit/restart program
+                if (input == 7)
+                {
+                    Console.Clear();
+                    break;
+                }
+                else if (input == 8)
+                {
+                    Console.Clear();
+                    Main(args);
+                }
+                else
+                {
+                    switch (input)          //performs specific methods based on user input
+                    {
+                        case (1):
+                            vPet.Feed();    //decreases hunger, increases thirst, increases potty
+                            Console.Clear();
+                            break;
+                        case (2):
+                            vPet.Water();
+                            Console.Clear();
+                            break;
+                        case (3):
+                            vPet.Poop();
+                            Console.Clear();
+                            break;
+                        case (4):
+                            vPet.Play();
+                            Console.Clear();
+                            break;
+                        case (5):
+                            vPet.Sleep();
+                            Console.Clear();
+                            break;
+                        case (6):
+                            vPet.Vet();
+                            Console.Clear();
+                            break;
+                        default:
+                            Console.WriteLine("Please enter a valid number");
+                            break;
+                    }
+                }
+
+                //alerts if pet is fed too much
+                if (vPet.Hunger < 0 && vPet.Hunger >= -10)
+                {
+                    Console.WriteLine("ALERT!\nYour pet does not need any more food!");
+                }
+                else if (vPet.Hunger < -10 && vPet.Hunger >= -15)
+                {
+                    Console.WriteLine("ALERT!\nYour pet needs a doctor!");
+
+                    Random num = new Random();  //pet becomes sick if it is fed too much
+                    int sickUpdate = num.Next(1, 3);
+                    vPet.Health -= sickUpdate;
+                }
+                else if (vPet.Hunger < -15)
+                {
+                    Random num = new Random();  //pet becomes sick if it is fed too much
+                    int sickUpdate = num.Next(5, 9);
+                    vPet.Health -= sickUpdate;
+                }
+
+                //alerts user to take pet to the vet
+                if (vPet.Health < 0 && vPet.Health >= -15)
+                {
+                    Console.WriteLine("ALERT!\nYour bet needs a doctor!");
+                }
+                else if (vPet.Health < -15)
+                {
+                    break;
+                }
+            }
+            //message to be displayed if user quits program
+            Console.WriteLine("You have lost your virtual pet.\n");
         }
     }
 }
---
+++ VirtualPet/Roscoe.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class Roscoe
+    {
+        //fields
+        private int hunger;
+        private int thirst;
+        private int energy;
+        private int potty;
+        private int tired;
+        private int health;
+
+        //properties
+        public int Hunger
+        {
+            get { return hunger; }
+            set { hunger = value; }
+        }
+        public int Thirst
+        {
+            get { return thirst; }
+            set { thirst = value; }
+        }
+        public int Energy
+        {
+            get { return energy; }
+            set { energy = value; }
+        }
+        public int Potty
+        {
+            get { return potty; }
+            set { potty = value; }
+        }
+        public int Tired
+        {
+            get { return tired; }
+            set { tired = value; }
+        }
+        public int Health
+        {
+            get { return health; }
+            set { health = value; }
+        }
+
+        //methods
+        public int Tick()
+        {
+            //create array of fields
+            int[] stats = { hunger, thirst, energy, tired, health };
+
+            Random num = new Random();
+            //randomly select one field
+            int randIndex = num.Next(0, stats.Length);
+            int randField = stats[randIndex];
+
+            int tick = num.Next(0, 10 + 1);
+
+            return randField += tick;
+        }
+        public void Feed()
+        {
+            //creates arbitrary random variable
+            Random num = new Random();
+
+            //updates hunger by random num ranging from 5-10
+            int hungerUpdate = num.Next(5, 11);
+            hunger -= hungerUpdate;
+
+            //updates thirst by random num ranging from 1-2
+            int thirstUpdate = num.Next(1, 3);
+            thirst += thirstUpdate;
+
+            //updates potty by random number ranging from 5-10
+            int pottyUpdate = num.Next(5, 11);
+            potty += pottyUpdate;
+
+            //updates sleepiness by random num ranging from 1-2
+            int tiredUpdate = num.Next(1, 3);
+            tired += tiredUpdate;
+        }
+        public void Water()
+        {
+            //creates arbitrary random variable
+            Random num = new Random();
+
+            //updates thirst by random num ranging from 5-10
+            int thirstUpdate = num.Next(5, 11);
+            thirst -= thirstUpdate;
+
+            //updates potty by random number ranging from 5-10
+            int pottyUpdate = num.Next(5, 11);
+            potty += pottyUpdate;
+
+            //updates sleepiness by random num ranging from 1-2
+            int tiredUpdate = num.Next(1, 3);
+            tired += tiredUpdate;
+        }
+        public void Poop()
+        {
+            //creates arbitrary random variable
+            Random num = new Random();
+
+            //updates potty by random number ranging from 5-10
+            int pottyUpdate = num.Next(5, 11);
+            potty -= pottyUpdate;
+
+            //updates energy by random number ranging from 5-10
+            int energyUpdate = num.Next(5, 11);
+            energy += energyUpdate;
+
+            //updates sleepiness by random num ranging from 1-2
+            int tiredUpdate = num.Next(1, 3);
+            tired += tiredUpdate;
+        }
+        public void Play()
+        {
+            //creates arbitrary random variable
+            Random num = new Random();
+
+            //updates potty by random number ranging from 1-2
+            int pottyUpdate = num.Next(1, 3);
+            potty += pottyUpdate;
+
+            //updates energy by random number ranging from 5-10
+            int energyUpdate = num.Next(5, 11);
+            energy -= energyUpdate;
+
+            //updates thirst by random num ranging from 1-5
+            int thirstUpdate = num.Next(1, 6);
+            thirst += thirstUpdate;
+
+            //updates sleepiness by random num ranging from 1-5
+            int tiredUpdate = num.Next(1, 6);
+            tired += tiredUpdate;
+        }
+        public void Sleep()
+        {
+            //resets sleepiness
+            tired = 0;
+            
+            //creates arbitrary random variable
+            Random num = new Random();
+
+            //updates energy by random num ranging from 1-10
+            if (energy > 10)
+            {
+                int energyUpdate = num.Next(1, 11);
+                energy -= energyUpdate;
+            }
+            else
+            {
+                energy = 0;
+            }
+
+            //updates potty by random number ranging from 1-2
+            int pottyUpdate = num.Next(1, 3);
+            potty += pottyUpdate;
+
+            //updates thirst by random num ranging from 1-3
+            int thirstUpdate = num.Next(1, 4);
+            thirst += thirstUpdate;
+
+            //updates health by random num ranging from 1-2
+            int healthUpdate = num.Next(1, 3);
+            health += healthUpdate;
+        }
+        public void Vet()
+        {
+            hunger = 0;
+            thirst = 0;
+            energy = 0;
+            potty = 0;
+            health = 0;
+            tired = 10;
+        }
+    }
+}
