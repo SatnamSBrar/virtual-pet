b721d9c49cfd4797f3ae70a0efafbf3ac0a14ffc
Satnam Brar <satnamsbrar@gmail.com>
2017-01-20 19:32:17 -0500

    version 1.2 - pet gets sick if neglected

--- VirtualPet/Program.cs
+++ VirtualPet/Program.cs
@@ -96,15 +96,18 @@ namespace VirtualPet
                             break;
                     }
                 }
+                //ALERTS
 
                 //alerts if pet is fed too much
                 if (vPet.Hunger < 0 && vPet.Hunger >= -10)
                 {
-                    Console.WriteLine("ALERT!\nYour pet does not need any more food!");
+                    Console.WriteLine("            ********************   ALERT!   ********************");
+                    Console.WriteLine("*****************   Your pet does not need any more food!   *****************");
                 }
                 else if (vPet.Hunger < -10 && vPet.Hunger >= -15)
                 {
-                    Console.WriteLine("ALERT!\nYour pet needs a doctor!");
+                    Console.WriteLine("       ********************   ALERT!   ********************");
+                    Console.WriteLine("*****************   Your pet needs a doctor!   *****************");
 
                     Random num = new Random();  //pet becomes sick if it is fed too much
                     int sickUpdate = num.Next(1, 3);
@@ -120,15 +123,112 @@ namespace VirtualPet
                 //alerts user to take pet to the vet
                 if (vPet.Health < 0 && vPet.Health >= -15)
                 {
-                    Console.WriteLine("ALERT!\nYour bet needs a doctor!");
+                    Console.WriteLine("       ********************   ALERT!   ********************");
+                    Console.WriteLine("*****************   Your pet needs a doctor!   *****************");
                 }
                 else if (vPet.Health < -15)
                 {
                     break;
                 }
+
+                //alerts user pet needs to potty
+                if (vPet.Potty > 15 && vPet.Potty <= 20)
+                {
+                    Console.WriteLine("       ********************   ALERT!   ********************");
+                    Console.WriteLine("*****************   Your pet needs to potty!   *****************");
+                }
+                else if (vPet.Potty > 20)
+                {
+                    Console.WriteLine("       ********************   ALERT!   ********************");
+                    Console.WriteLine("*********   Your pet has eaten its poop and is now sick!   *********");
+                    Random num = new Random();  //pet becomes sick if it eats poop
+                    int sickUpdate = num.Next(5, 9);
+                    vPet.Health -= sickUpdate;
+                }
+
+                //alerts user if pet is too tired
+                if (vPet.Tired > 15 && vPet.Tired <= 25)
+                {
+                    Console.WriteLine("       ********************   ALERT!   ********************");
+                    Console.WriteLine("*****************   Your pet is getting tired!   *****************");
+                }
+                else if (vPet.Tired > 25)
+                {
+                    Console.WriteLine("       ********************   ALERT!   ********************");
+                    Console.WriteLine("*****************   Your pet has fallen asleep!   *****************");
+                    Random num = new Random();  //pet falls asleep if too tired
+                    int tiredUpdate = num.Next(10, 20);
+                    vPet.Tired -= tiredUpdate;
+                }
+
+                //alerts user if pet has too much energy
+                if (vPet.Energy > 20 && vPet.Energy <= 30)
+                {
+                    Console.WriteLine("      ********************   ALERT!   ********************");
+                    Console.WriteLine("*****************   Your pet needs to play!   *****************");
+                }
+                else if (vPet.Energy > 30)
+                {
+                    Console.WriteLine("     ********************   ALERT!   ********************");
+                    Console.WriteLine("*****************   Your pet hurt itself!   *****************");
+                    Random num = new Random();  //pet gets hurt if it runs around too much
+                    int sickUpdate = num.Next(4);
+                    vPet.Health -= sickUpdate;
+                }
+
+                //alerts if pet needs to be fed
+                if (vPet.Hunger > 20 && vPet.Hunger <=30)
+                {
+                    Console.WriteLine("      ********************   ALERT!   ********************");
+                    Console.WriteLine("*****************   Your pet needs to eat!   *****************");
+                }
+                else if (vPet.Hunger > 30)
+                {
+                    Console.WriteLine("    ********************   ALERT!   ********************");
+                    Console.WriteLine("*****************   Your pet is sick!   *****************");
+                    Random num = new Random();  //pet gets sick if it doesn't eat
+                    int sickUpdate = num.Next(5, 11);
+                    vPet.Health -= sickUpdate;
+                }
+
+                //alerts if pet needs water
+                if (vPet.Hunger > 20 && vPet.Hunger <= 30)
+                {
+                    Console.WriteLine("      ********************   ALERT!   ********************");
+                    Console.WriteLine("*****************   Your pet needs water!   *****************");
+                }
+                else if (vPet.Hunger > 30)
+                {
+                    Console.WriteLine("    ********************   ALERT!   ********************");
+                    Console.WriteLine("*****************   Your pet is sick!   *****************");
+                    Random num = new Random();  //pet gets sick if deprived of water
+                    int sickUpdate = num.Next(5, 11);
+                    vPet.Health -= sickUpdate;
+                }
             }
-            //message to be displayed if user quits program
-            Console.WriteLine("You have lost your virtual pet.\n");
+            //random message to be displayed if user quits program
+            Random conclude = new Random();
+            int conclusion = conclude.Next(4);
+
+            switch(conclusion)
+            {
+                case (0):
+                    Console.WriteLine("\nYou have lost your virtual pet.\n");
+                    break;
+                case (1):
+                    Console.WriteLine("\nYou need to be more responsble.\n");
+                    break;
+                case (2):
+                    Console.WriteLine("\nYour poor virtual pet :(\n");
+                    break;
+                case (3):
+                    Console.WriteLine("\nGame Over.\n");
+                    break;
+                case (4):
+                    Console.WriteLine("\nYour virtual pet as died as a consequence of your ineptitude.\n");
+                    break;
+            }
+            
         }
     }
 }
