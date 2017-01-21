f3091a91ef50f5cbccb8edd2eb421aa9e057b659
Satnam Brar <satnamsbrar@gmail.com>
2017-01-20 20:46:36 -0500

    first attempt to prompt user to maximize screen

--- VirtualPet/Program.cs
+++ VirtualPet/Program.cs
@@ -21,10 +21,17 @@ namespace VirtualPet
             vPet.Tired = 0;
             vPet.Health = 0;
 
+            //sets bool for window maximize prompt
+            bool first = true;
+
             while (true)
             {
-                //asks user to maximize console window for optimal experience
-                Console.WriteLine("Please maximize window for optimal experience\n");
+                //asks user to maximize console window for optimal experience the first time program is used
+                if (first == true)
+                {
+                    Console.WriteLine("PLEASE MAXIMIZE WINDOW FOR OPTIMAL EXPERIENCE");
+                }
+                first = false;
 
                 //displays pet status
                 Console.WriteLine("Roscoe's Health:");
@@ -59,7 +66,7 @@ namespace VirtualPet
                         Console.WriteLine(@" \_,` | \-'  /   )`-')");
                         Console.WriteLine("  \"\") `\"`    \\  ((`\"`");
                         Console.WriteLine(" ___Y  ,    .'7 /|");
-                        Console.WriteLine("(_,___/...-` (_/_/");
+                        Console.Write("(_,___/...-` (_/_/\t\t\t\t\t\t");
                         break;
                     case (1):
                         Console.WriteLine("    _____^_");
@@ -67,7 +74,7 @@ namespace VirtualPet
                         Console.WriteLine(@"    \   /  ^ |");
                         Console.WriteLine(@"   / \_/   0  \");
                         Console.WriteLine(@"  /    ____     0");
-                        Console.WriteLine(@" /     /  \___ _/");
+                        Console.Write(" /     /  \\___ _/\t\t\t\t\t\t");
                         break;
                     case (2):
                         Console.WriteLine(@"     |\_/|");
@@ -76,21 +83,21 @@ namespace VirtualPet
                         Console.WriteLine(@"     |  _/\------____ ((| |))");
                         Console.WriteLine(@"     |               `--' | ");
                         Console.WriteLine(@" ____|_       ___|   |___.'");
-                        Console.WriteLine(@"/_/_____/____/_______|");
+                        Console.Write("/_/_____/____/_______|\t\t\t\t\t\t");
                         break;
                     case (3):
                         Console.WriteLine(@"   __");
                         Console.WriteLine(@"o-''|\_____/)");
                         Console.WriteLine(@" \_/|_)     )");
                         Console.WriteLine(@"    \  __  /");
-                        Console.WriteLine(@"    (_/ (_/");
+                        Console.Write("    (_/ (_/\t\t\t\t\t\t");
                         break;
                     case (4):
                         Console.WriteLine("             .--~~,__");
                         Console.WriteLine(":-....,-------`~~'._.'");
                         Console.WriteLine(" `-,,,  ,_      ;'~U'");
                         Console.WriteLine("  _,-' ,'`-__; '--.");
-                        Console.WriteLine(" (_/'~~      ''''(;");
+                        Console.Write(" (_/'~~      ''''(;\t\t\t\t\t\t");
                         break;
                 }
 
@@ -171,12 +178,12 @@ namespace VirtualPet
                 if (vPet.Hunger < 0 && vPet.Hunger >= -10)
                 {
                     Console.WriteLine("            ********************   ALERT!   ********************");
-                    Console.WriteLine("*****************   Your pet does not need any more food!   *****************");
+                    Console.WriteLine("*****************   Your pet does not need any more food!   *****************\n");
                 }
                 else if (vPet.Hunger < -10 && vPet.Hunger >= -15)
                 {
                     Console.WriteLine("       ********************   ALERT!   ********************");
-                    Console.WriteLine("*****************   Your pet needs a doctor!   *****************");
+                    Console.WriteLine("*****************   Your pet needs a doctor!   *****************\n");
 
                     Random num = new Random();  //pet becomes sick if it is fed too much
                     int sickUpdate = num.Next(1, 3);
@@ -193,7 +200,7 @@ namespace VirtualPet
                 if (vPet.Health < 0 && vPet.Health >= -15)
                 {
                     Console.WriteLine("       ********************   ALERT!   ********************");
-                    Console.WriteLine("*****************   Your pet needs a doctor!   *****************");
+                    Console.WriteLine("*****************   Your pet needs a doctor!   *****************\n");
                 }
                 else if (vPet.Health < -15)
                 {
@@ -204,12 +211,12 @@ namespace VirtualPet
                 if (vPet.Potty > 15 && vPet.Potty <= 20)
                 {
                     Console.WriteLine("       ********************   ALERT!   ********************");
-                    Console.WriteLine("*****************   Your pet needs to potty!   *****************");
+                    Console.WriteLine("*****************   Your pet needs to potty!   *****************\n");
                 }
                 else if (vPet.Potty > 20)
                 {
                     Console.WriteLine("       ********************   ALERT!   ********************");
-                    Console.WriteLine("*********   Your pet has eaten its poop and is now sick!   *********");
+                    Console.WriteLine("*********   Your pet has eaten its poop and is now sick!   *********\n");
                     Random num = new Random();  //pet becomes sick if it eats poop
                     int sickUpdate = num.Next(5, 9);
                     vPet.Health -= sickUpdate;
@@ -219,12 +226,12 @@ namespace VirtualPet
                 if (vPet.Tired > 15 && vPet.Tired <= 25)
                 {
                     Console.WriteLine("       ********************   ALERT!   ********************");
-                    Console.WriteLine("*****************   Your pet is getting tired!   *****************");
+                    Console.WriteLine("*****************   Your pet is getting tired!   *****************\n");
                 }
                 else if (vPet.Tired > 25)
                 {
                     Console.WriteLine("       ********************   ALERT!   ********************");
-                    Console.WriteLine("*****************   Your pet has fallen asleep!   *****************");
+                    Console.WriteLine("*****************   Your pet has fallen asleep!   *****************\n");
                     Random num = new Random();  //pet falls asleep if too tired
                     int tiredUpdate = num.Next(10, 20);
                     vPet.Tired -= tiredUpdate;
@@ -234,12 +241,12 @@ namespace VirtualPet
                 if (vPet.Energy > 20 && vPet.Energy <= 30)
                 {
                     Console.WriteLine("      ********************   ALERT!   ********************");
-                    Console.WriteLine("*****************   Your pet needs to play!   *****************");
+                    Console.WriteLine("*****************   Your pet needs to play!   *****************\n");
                 }
                 else if (vPet.Energy > 30)
                 {
                     Console.WriteLine("     ********************   ALERT!   ********************");
-                    Console.WriteLine("*****************   Your pet hurt itself!   *****************");
+                    Console.WriteLine("*****************   Your pet hurt itself!   *****************\n");
                     Random num = new Random();  //pet gets hurt if it runs around too much
                     int sickUpdate = num.Next(4);
                     vPet.Health -= sickUpdate;
@@ -249,12 +256,12 @@ namespace VirtualPet
                 if (vPet.Hunger > 20 && vPet.Hunger <=30)
                 {
                     Console.WriteLine("      ********************   ALERT!   ********************");
-                    Console.WriteLine("*****************   Your pet needs to eat!   *****************");
+                    Console.WriteLine("*****************   Your pet needs to eat!   *****************\n");
                 }
                 else if (vPet.Hunger > 30)
                 {
                     Console.WriteLine("    ********************   ALERT!   ********************");
-                    Console.WriteLine("*****************   Your pet is sick!   *****************");
+                    Console.WriteLine("*****************   Your pet is sick!   *****************\n");
                     Random num = new Random();  //pet gets sick if it doesn't eat
                     int sickUpdate = num.Next(5, 11);
                     vPet.Health -= sickUpdate;
@@ -264,12 +271,12 @@ namespace VirtualPet
                 if (vPet.Hunger > 20 && vPet.Hunger <= 30)
                 {
                     Console.WriteLine("      ********************   ALERT!   ********************");
-                    Console.WriteLine("*****************   Your pet needs water!   *****************");
+                    Console.WriteLine("*****************   Your pet needs water!   *****************\n");
                 }
                 else if (vPet.Hunger > 30)
                 {
                     Console.WriteLine("    ********************   ALERT!   ********************");
-                    Console.WriteLine("*****************   Your pet is sick!   *****************");
+                    Console.WriteLine("*****************   Your pet is sick!   *****************\n");
                     Random num = new Random();  //pet gets sick if deprived of water
                     int sickUpdate = num.Next(5, 11);
                     vPet.Health -= sickUpdate;
@@ -285,46 +292,51 @@ namespace VirtualPet
                     Console.WriteLine("\nYou have lost your virtual pet.\n");
                     Console.WriteLine(@"      _    _");
                     Console.WriteLine(@"     / )__/ )");
-                    Console.WriteLine(@"    /      _\ _");
-                    Console.WriteLine(@"   (______( |/ \");
-                    Console.WriteLine(@"   (/      \|xx-o");
-                    Console.WriteLine(@"             vv\n");
+                    Console.WriteLine(@"    /      _\ __");
+                    Console.WriteLine(@"   (______( |/  \");
+                    Console.WriteLine(@"   (/      \|xx--o");
+                    Console.WriteLine(@"             vv");
+                    Console.WriteLine("\n");
                     break;
                 case (1):
                     Console.WriteLine("\nYou need to be more responsble.\n");
                     Console.WriteLine(@"      _    _");
                     Console.WriteLine(@"     / )__/ )");
-                    Console.WriteLine(@"    /      _\ _");
-                    Console.WriteLine(@"   (______( |/ \");
-                    Console.WriteLine(@"   (/      \|xx-o");
-                    Console.WriteLine(@"             vv\n");
+                    Console.WriteLine(@"    /      _\ __");
+                    Console.WriteLine(@"   (______( |/  \");
+                    Console.WriteLine(@"   (/      \|xx--o");
+                    Console.WriteLine(@"             vv");
+                    Console.WriteLine("\n");
                     break;
                 case (2):
                     Console.WriteLine("\nYour poor virtual pet :(\n");
                     Console.WriteLine(@"      _    _");
                     Console.WriteLine(@"     / )__/ )");
-                    Console.WriteLine(@"    /      _\ _");
-                    Console.WriteLine(@"   (______( |/ \");
-                    Console.WriteLine(@"   (/      \|xx-o");
-                    Console.WriteLine(@"             vv\n");
+                    Console.WriteLine(@"    /      _\ __");
+                    Console.WriteLine(@"   (______( |/  \");
+                    Console.WriteLine(@"   (/      \|xx--o");
+                    Console.WriteLine(@"             vv");
+                    Console.WriteLine("\n");
                     break;
                 case (3):
                     Console.WriteLine("\nGame Over.\n");
                     Console.WriteLine(@"      _    _");
                     Console.WriteLine(@"     / )__/ )");
-                    Console.WriteLine(@"    /      _\ _");
-                    Console.WriteLine(@"   (______( |/ \");
-                    Console.WriteLine(@"   (/      \|xx-o");
-                    Console.WriteLine(@"             vv\n");
+                    Console.WriteLine(@"    /      _\ __");
+                    Console.WriteLine(@"   (______( |/  \");
+                    Console.WriteLine(@"   (/      \|xx--o");
+                    Console.WriteLine(@"             vv");
+                    Console.WriteLine("\n");
                     break;
                 case (4):
                     Console.WriteLine("\nYour virtual pet as died as a consequence of your ineptitude.\n");
                     Console.WriteLine(@"      _    _");
                     Console.WriteLine(@"     / )__/ )");
-                    Console.WriteLine(@"    /      _\ _");
-                    Console.WriteLine(@"   (______( |/ \");
-                    Console.WriteLine(@"   (/      \|xx-o");
-                    Console.WriteLine(@"             vv\n");
+                    Console.WriteLine(@"    /      _\ __");
+                    Console.WriteLine(@"   (______( |/  \");
+                    Console.WriteLine(@"   (/      \|xx--o");
+                    Console.WriteLine(@"             vv");
+                    Console.WriteLine("\n");
                     break;
             }
             
